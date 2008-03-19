e38308a4747aaf2bbde97d5f9f67afdd33a503d4
tekezo <devnull@localhost>
2008-03-20 01:10:35 +0900

    support pointing_relative2scroll_ratio

---
+++ src/kext/Config.hpp
@@ -0,0 +1,31 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+namespace org_pqrs_KeyRemap4MacBook {
+  class Config {
+  public:
+    Config() {
+      repeat_initial_wait = 500;
+      repeat_wait = 30;
+      pointing_relative2scroll_ratio = 750;
+    }
+
+    // ----------------------------------------
+    int repeat_initial_wait;
+    int repeat_wait;
+
+    int pointing_relative2scroll_ratio;
+
+#include "config/output/include.config.hpp"
+
+    // ----------------------------------------
+    int debug;
+    int debug_pointing;
+  };
+  extern Config config;
+
+  void sysctl_register(void);
+  void sysctl_unregister(void);
+}
+
+#endif
